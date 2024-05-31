65a81078266f5ba2abd64190f44e5bc3fcd78443
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-05-31 15:17:35 +0530

    added loading pages

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -30,6 +31,10 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
+    <>
+
+{isLoading && <LoadingModal/>}
+
     <div
       onClick={handleClick}
       className="
@@ -63,7 +68,7 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
           </div>
         </div>
       </div>
-    </div>
+    </div></>
   );
 };
 
